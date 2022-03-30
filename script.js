213ca3cb1a8134ac3a3d627b291252cb16d56d23
DarkmodeBrewing <65855102+DarkmodeBrewing@users.noreply.github.com>
2022-03-30 14:38:05 +0200

    Initial commit
    Small script, specialized in parsing out fruits and berries from the livsmedelsverket open data xml api

---
+++ script.js
@@ -0,0 +1,29 @@
+const { XMLParser } = require("fast-xml-parser");
+const fs = require("fs");
+
+const options = {
+  ignoreAttributes: false,
+};
+
+const xmlDataBuffer = fs.readFileSync("20220330.xml"); //http://www7.slv.se/apilivsmedel/LivsmedelService.svc/Livsmedel/Naringsvarde/<ÅÅÅÅMMDD>
+
+const xmlDataStr = xmlDataBuffer.toString();
+
+const parser = new XMLParser(options);
+const jsonObj = parser.parse(xmlDataStr);
+const fruktNyckel = "Frukt färsk fryst";
+const barNyckel = "Bär färska frysta";
+const sockerNyckel = "Socker totalt";
+const livsmedelslista = jsonObj.LivsmedelDataset.LivsmedelsLista.Livsmedel;
+const frukterOchBar = livsmedelslista.filter(
+  (item) => item.Huvudgrupp === fruktNyckel || item.Huvudgrupp === barNyckel
+);
+
+for (const [index, fruktBar] of frukterOchBar.entries()) {
+  const namn = fruktBar.Namn;
+  const viktGram = fruktBar.ViktGram;
+  const sockerMangd = fruktBar.Naringsvarden.Naringsvarde.find(
+    (naringsvarde) => naringsvarde.Namn === sockerNyckel
+  );
+  console.log(`${index + 1}: ${namn} ${sockerMangd.Varde}g per ${viktGram}g`);
+}
